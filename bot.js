66267898cd35f3826b30d2aa2e6db06a64644e7d
SevensWife <42893250+SevensWife@users.noreply.github.com>
2018-09-01 16:36:21 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,183 @@
+console.log('Eichi va a cazar');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+	if (message.content == 'Im sleepy') {
+		message.channel.sendMessage('Oh, are you flirting with me? So cute, fufu.');
+	}
+	if (message.content == 'Lets dance') {
+		message.channel.sendMessage('A cumbia, Miss?');
+	}
+	if (message.content == 'Fuck Kaoru') {
+		message.channel.sendMessage('Yes, fuck him!');
+	}
+	if (message.content == 'Im crying') {
+		message.channel.sendMessage('Poor girl, let me wipe your tears.');
+	}
+	if (message.content == 'I love you Wataru') {
+		message.channel.sendMessage('You mean it? Fufu, I do love you too.');
+	}
+	if (message.content == 'Do you like Eichi?') {
+		message.channel.sendMessage('Eichi Tenshouin? Yes, of course?');
+	}
+	if (message.content == 'Do you like Keito?') {
+		message.channel.sendMessage('Hes my fated rival, you know.');
+	}
+	if (message.content == 'Do you like Tori?') {
+		message.channel.sendMessage('Hime-kun is surely so cute, just like you, fufu.');
+	}
+	if (message.content == "!Please send me to sleep.") {
+		message.author.send("Hey, you, sleeping beauty, its time to sleep, will you come with me?");
+	}
+	if (message.content == 'Amazon') {
+		message.channel.sendMessage('AAAMAAAAAZINGU');
+	}
+	if (message.content == 'This is so sad Wataru play Despacito') {
+		message.channel.sendMessage('https://www.youtube.com/watch?v=kJQP7kiw5Fk');
+	}
+	if (message.content == 'Wataru Im sad') {
+		message.channel.sendMessage('Why are you sad? Do you want me to comfort you?');
+	}
+	if (message.content == 'Im hungry') {
+		message.channel.sendMessage('I wont let you eat me, fufu.');
+	}
+	if (message.content == 'Me cago en dios') {
+		message.channel.sendMessage('Hey, that vocabulary!');
+	}
+	if (message.content == 'joder') {
+		message.channel.sendMessage('Hey, thats not how a lady should talk!');
+	}
+	if (message.content == 'h') {
+		message.channel.sendMessage('I dont get what you are trying to say...');
+	}
+	if (message.content == 'hijo') {
+		message.channel.sendMessage('Its not like I wanna be rude, but, how many children do you have?');
+	}
+	if (message.content == 'wtf') {
+		message.channel.sendMessage('Wtf? What do you mean? W... to... Fine!? AMAZING!');
+	}
+	if (message.content == 'omg') {
+		message.channel.sendMessage('I wish I were your God, you seem to appreciate him so much, fufu. Im jealous.');
+	}
+	if (message.content == 'lmao') {
+		message.channel.sendMessage('...Mao Isara? Isnt that Ritsu Sakumas boyfriend?');
+	}
+	if (message.content == 'te follo wataru') {
+		message.channel.sendMessage('O-Oh... Im blushing... fufu...');
+	}
+	if (message.content == 'wapo') {
+		message.channel.sendMessage('Thank you! You are beautiful too.');
+	}
+	if (message.content == 'Wataru are you gay') {
+		message.channel.sendMessage('Isnt the sky blue?');
+	}
+	if (message.content == 'me cago en dios') {
+		message.channel.sendMessage('Moderate your vocabulary, my lady.');
+	}
+	if (message.content == 'xd') {
+		message.channel.sendMessage('Marian wouldnt like to read that, you know?');
+	}
+	if (message.content == ':v') {
+		message.channel.sendMessage('Uuuh... Marian is surely angry right now... you better run.');
+	}
+	if (message.content == 'lol') {
+		message.channel.sendMessage('Do you play League of Legends!? AMAZING!');
+	}
+	if (message.content == 'aw') {
+		message.channel.sendMessage('You are so cute when you make those sounds, you know? Fufu...');
+	}
+	if (message.content == 'Gracias Wataru') {
+		message.channel.sendMessage('Dont worry, its nothing. I would do anything for you.');
+	}
+	if (message.content == 'me cago en to') {
+		message.channel.sendMessage('My lady, please! You shouldnt talk like that.');
+	}
+	if (message.content == 'gracias Wataru') {
+		message.channel.sendMessage('Dont worry, its nothing. I would do anything for you.');
+	}
+	if (message.content == 'gracias wataru') {
+		message.channel.sendMessage('Dont worry, its nothing. I would do anything for you.');
+	}
+	if (message.content == 'Gracias wataru') {
+		message.channel.sendMessage('Dont worry, its nothing. I would do anything for you.');
+	}
+	if (message.content == 'a tope') {
+		message.channel.sendMessage('SUBARASHI!!');
+	}
+	if (message.content == 'A tope') {
+		message.channel.sendMessage('SUBARASHI!!');
+	}
+	if (message.content == 'i love you wataru') {
+		message.channel.sendMessage('I love you too, honey.');
+	}
+	if (message.content == 'I love you wataru') {
+		message.channel.sendMessage('Youre so sweet... Im melting.');
+	}
+	if (message.content == '???') {
+		message.channel.sendMessage('Are you doubting?');
+	}
+	if (message.content == 'te quiero wataru') {
+		message.channel.sendMessage('So sweet, fufu...');
+	}
+	if (message.content == 'Te quiero wataru') {
+		message.channel.sendMessage('I love how it sounds in Spanish, te quiero, fufu.');
+	}
+	if (message.content == 'TE QUIERO WATARU') {
+		message.channel.sendMessage('I LOVE YOU TOO HONEY');
+	}
+	if (message.content == 'casate conmigo wataru') {
+		message.channel.sendMessage('O-Oh... such a sudden request... fufufu... Im blushing.');
+	}
+	if (message.content == '?') {
+		message.channel.sendMessage('Is there something you dont understand, cutie?');
+	}
+	if (message.content == 'ah') {
+		message.channel.sendMessage('Ella quiere uhm ah uhm.');
+	}
+	if (message.content == 'po no') {
+		message.channel.sendMessage('I think so too, fufu.');
+	}
+	if (message.content == 'hola') {
+		message.channel.sendMessage('Buenas! Fufu... how is my pronunciation, honey?');
+	}
+	if (message.content == 'Hola') {
+		message.channel.sendMessage('Hello! Im here too!');
+	}
+	if (message.content == 'warra') {
+		message.channel.sendMessage('Oh, please, dont insult each other! There is Wataru for everyone!');
+	}
+	if (message.content == 'adios') {
+		message.channel.sendMessage('See ya, cutie!');
+	}
+	if (message.content == 'buenos dias') {
+		message.channel.sendMessage('Good morning, honey! How are you?');
+	}
+	if (message.content == 'hostias') {
+		message.channel.sendMessage('Hey!! Please, that vocabulary! Im gonna get angry, you know... fufu.');
+	}
+	if (message.content == 'hostia') {
+		message.channel.sendMessage('...If you dont stop, Im gonna punish you. Fufufu...');
+	}
+	if (message.content == 'hajime') {
+		message.channel.sendMessage('Isnt that a poor boy? Like, literally.');
+	}
+	if (message.content == 'ah enga') {
+		message.channel.sendMessage('You finally understood, then!');
+	}
+	if (message.content == 'ok') {
+		message.channel.sendMessage('You all have such a small vocabulary, you know that? Maybe I could teach you some other words if you let me, fufufu...');
+	}
+	if (message.content == 'im gay') {
+		message.channel.sendMessage('Me too! Lets dance to Rainbow Circus together! https://www.youtube.com/watch?v=nyWBAX3sNSk');
+	}
+	if (message.content == '.') {
+		message.channel.sendMessage('Whats wrong? Do you need a hug? Im available, just in case...');
+	}
+	if (message.content == 'help') {
+		message.channel.sendMessage('Do you need help? Im here for you, my lady!');
+	}
+});
+bot.login('NDg1MTIyNzU3NzM1MjE5MjIw.DmtFFQ.1unzz9YLjE6ZJ3v8414VN1xgJTw');
+
+client.login(process.env.BOT_TOKEN);
